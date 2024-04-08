e95c48ad9d5c019e83605127dccefe313342c9c7
Gsidhu724622 <166370416+Gsidhu724622@users.noreply.github.com>
2024-04-08 13:39:06 +0000

    Arre

---
+++ 01_basic/Arr.js
@@ -0,0 +1,31 @@
+// array
+const myArr = [0, 1, 2, 3, 4, 5]
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(3));
+// const newArr = myArr.join();
+
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// Slice Splice
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log("A", myArr)
+
+
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2);
+// console.log("B", myArr)
+
+// slice just give the value with the range include no change in the original array
+// but splice give the range value include also change the original array 
+// [ 1, 2 ]
+// A [ 0, 1, 2, 3, 4, 5 ]
+// [ 1, 2, 3 ]
+// B [ 0, 4, 5 ]
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)  in millisecond 
+console.log(Math.floor(Date.now()/1000)); // converted in seconds
+
+console.log()
